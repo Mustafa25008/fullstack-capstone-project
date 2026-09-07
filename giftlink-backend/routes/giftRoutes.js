a10a57795a1a98e4eeee55f75fcbb0565d42910a
Mustafa25008 <mustafa25008c@gmail.com>
2026-09-07 07:37:58 -0400

    update file

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,4 +1,7 @@
-router.get('/', async (req, res) => {
+/*jshint esversion: 8 */
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
